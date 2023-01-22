a687894482d56569e1fbf8cf2287658e1508d071
Esley <esleynathan@hotmail.com>
2023-01-22 18:34:39 -0300

    Django Admin | Instrodução a area Adminstrativa e Configurando para PT-BR

---
+++ autenticacao/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
+from .models import Pessoa, Cargos
+from django.shortcuts import get_list_or_404
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro/index.html')
+
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')        
+        senha = request.POST.get('senha')
+        pessoa = Pessoa(nome = nome,
+                        email = email,
+                        senha = senha
+                        )
+
+        pessoa.save()
+
+        return HttpResponse('Você foi cadastrado.')
+
+def listar(request):    
+    pessoas = Pessoa.objects.all()
+    return render(request, 'listar/listar.html', {'pessoas': pessoas})
+
+def listar_unico(request, id):
+    pessoa = get_list_or_404(Pessoa, id = id)
+    print(pessoa)
+    return render(request, 'listar/listar.html', {'pessoas': pessoa})
